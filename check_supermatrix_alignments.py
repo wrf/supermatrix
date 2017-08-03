25ccf0a3f1114f68db2ed556cd27b2c40e02fd16
wfrancis <wfrancis@biology.sdu.dk>
2017-08-03 13:49:32 +0200

    allows gz, makes matrix occupancy file

--- check_supermatrix_alignments.py
+++ check_supermatrix_alignments.py
@@ -2,7 +2,7 @@
 #
 # check_supermatrix_alignments.py created 2017-03-13
 
-'''check_supermatrix_alignments.py v1.0 2017-03-27
+'''check_supermatrix_alignments.py v1.1 2017-08-03
 tool to quickly check for abnormal sequences in fasta alignments
 
 checknogalignments.py -a matrix.phy -p partitions.txt
@@ -10,14 +10,22 @@ checknogalignments.py -a matrix.phy -p partitions.txt
     for partitioned alignments, formats (-f) include:
   clustal, fasta, nexus, phylip, phylip-relaxed, stockholm
 
+    large matrices can be gzipped, as .gz
+
     output consists of tab delimited fields:
 Species  Partitions  Number-missing  Percent-missing  Number-partial  Percent-partial
+
+    for optional occupancy matrix file:
+checknogalignments.py -a matrix.phy -p partitions.txt -m occupancy_matrix.tab
+    where matrix consists of three values for:
+    present (2), partial (1), and absent (0)
 '''
 
 import sys
 import os
 import argparse
 import time
+import gzip
 from collections import defaultdict,Counter
 from Bio import AlignIO
 
@@ -34,25 +42,42 @@ def get_partitions(partitionfile):
 	print >> sys.stderr, "# read {} partitions from {}".format(len(partitions), partitionfile), time.asctime()
 	return partitions
 
-def check_alignments(fullalignment, alignformat, partitions):
+def check_alignments(fullalignment, alignformat, partitions, makematrix=False):
 	'''read large alignment, return the dict where key is species and value is number of gap-only sequences'''
 	gapdict = {} # must set all values to zero in order to not skip full taxa
 	halfgapdict = defaultdict(int)
-	alignedseqs = AlignIO.read(fullalignment, alignformat)
+
+	occmatrix = [] if makematrix else None
+
+	if fullalignment.rsplit('.',1)[1]=="gz": # autodetect gzip format
+		opentype = gzip.open
+		print >> sys.stderr, "# reading alignment {} as gzipped".format(fullalignment), time.asctime()
+	else: # otherwise assume normal open for GTF format
+		opentype = open
+		print >> sys.stderr, "# reading alignment {}".format(fullalignment), time.asctime()
+	alignedseqs = AlignIO.read(opentype(fullalignment), alignformat)
 	for seqrec in alignedseqs: # all species must be counted once
 		gapdict[seqrec.id] = 0
+		if occmatrix is not None:
+			occmatrix.append([seqrec.id]) # initate each species as a new list with ID
 	for part in partitions:
-		alignpart = alignedseqs[:, part[0]-1:part[1] ]
-		for seqrec in alignpart:
+		alignpart = alignedseqs[:, part[0]-1:part[1] ] # alignment of each partition only
+		for i,seqrec in enumerate(alignpart):
 			species = seqrec.id
 			seqlen = len(seqrec.seq)
 			lettercounts = Counter(str(seqrec.seq).replace("X","-"))
+			occupancyscore = 2 # by default is present, reassign if absent or partial
 			if lettercounts["-"] == seqlen: # seq is all gaps, so no seq
 				gapdict[species] += 1
+				occupancyscore = 0 # set to 0 if all gaps
 			elif lettercounts["-"] >= seqlen/2:
 				halfgapdict[species] += 1
+				occupancyscore = 1 # set to 1 if partial
+
+			if occmatrix: # if building matrix, add that value to the matrix at sequence i
+				occmatrix[i].append(str(occupancyscore))
 	print >> sys.stderr, "# split {} taxa alignment by partitions".format(len(gapdict)), time.asctime()
-	return gapdict, halfgapdict
+	return gapdict, halfgapdict, occmatrix
 
 def main(argv, wayout):
 	if not len(argv):
@@ -61,12 +86,24 @@ def main(argv, wayout):
 	parser.add_argument('-a','--alignment', help="supermatrix alignment")
 	parser.add_argument('-f','--format', default="fasta", help="alignment format [fasta]")
 	parser.add_argument('-H','--header', action="store_true", help="include header line")
+	parser.add_argument('-m','--matrix-out', help="name for optional matrix-occupancy output file")
+	parser.add_argument('-D','--matrix-delimiter', default="\t", help="delimiter for matrix file [default is tab]")
 	parser.add_argument('-p','--partition', help="partition file for splitting large alignments")
 	args = parser.parse_args(argv)
 
 	partitions = get_partitions(args.partition)
-	gapdict, halfgaps = check_alignments(args.alignment, args.format, partitions)
+	gapdict, halfgaps, occmatrix = check_alignments(args.alignment, args.format, partitions, args.matrix_out)
 	numparts = len(partitions)
+
+	if args.matrix_out and occmatrix:
+		print >> sys.stderr, "# writing matrix to {}".format(args.matrix_out), time.asctime()
+		with open(args.matrix_out,'w') as mo:
+			# generate header line
+			headerline = ["Species"] + ["{}-{}".format(*part) for part in partitions]
+			print >> mo, args.matrix_delimiter.join(headerline)
+			# print occupancy by each species
+			for occbysplist in occmatrix:
+				print >> mo, args.matrix_delimiter.join(occbysplist)
 
 	if args.header:
 		#                  0           1           2       3
